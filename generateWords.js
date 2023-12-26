9dac176109e36e5c7ecd0b5685f2f647d0032e47
Gabriella Mondvay <gabriellamondvay@gmail.com>
2023-12-26 21:19:38 +0100

    Remove old function

--- generateWords.js
+++ generateWords.js
@@ -3,14 +3,6 @@ import adverbs from "./words/adverbs.js";
 import nouns from "./words/nouns.js";
 import verbs from "./words/verbs.js";
 
-const generateSentence = () => {
-  let randomAdj = adjectives[Math.floor(Math.random() * adjectives.length)];
-  let randomAdverb = adverbs[Math.floor(Math.random() * adverbs.length)];
-  let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-  let randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
-  return `I want to ${randomAdverb} ${randomVerb} you, you ${randomAdj} ${randomNoun}`;
-};
-
 const generateVerb = () => {
   let randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
   return randomVerb;
